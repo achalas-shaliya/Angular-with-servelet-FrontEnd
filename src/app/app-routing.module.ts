72a33f68e6968ee764e2adefaa492bb79ea63e87
achalas-shaliya <achalashaliya77@gmail.com>
2019-06-18 19:26:25 +0530

    enable routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,36 @@
-import {NgModule} from '@angular/core';
-import {RouterModule, Routes} from '@angular/router';
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { ManageCustomersComponent } from './view/manage-customers/manage-customers.component';
+import { ManageItemComponent } from './view/manage-item/manage-item.component';
+import { ManageOrderComponent } from './view/manage-order/manage-order.component';
+import { DashboardComponent } from './view/dashboard/dashboard.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'dashboard',
+    component: DashboardComponent
+  },
+  {
+    path: 'customers',
+    component: ManageCustomersComponent
+  },
+  {
+    path: 'items',
+    component: ManageItemComponent
+  },
+  {
+    path: 'orders',
+    component: ManageOrderComponent
+  },
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: 'dashboard'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule {
-}
+export class AppRoutingModule { }
