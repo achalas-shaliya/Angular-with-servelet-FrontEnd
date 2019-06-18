9d24bb0954fc1871a816cb69084357e2dff6db50
SenelithPerera <senelith2018@gmail.com>
2019-06-18 19:14:45 +0530

    adding customer component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,18 @@
-import {BrowserModule} from '@angular/platform-browser';
-import {NgModule} from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
 
-import {AppRoutingModule} from './app-routing.module';
-import {AppComponent} from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
 import { SidebarComponent } from './view/sidebar/sidebar.component';
 import { DashboardComponent } from './view/dashboard/dashboard.component';
+import { ManageCustomersComponent } from './view/manage-customers/manage-customers.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     SidebarComponent,
-    DashboardComponent
+    DashboardComponent,
+    ManageCustomersComponent
   ],
   imports: [
     BrowserModule,
