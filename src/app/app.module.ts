5ea83ff739e527f7f444c52864d17328703cbc76
achalas-shaliya <shaliya20761>
2019-06-18 12:20:30 +0530

    adding dashboard

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import {NgModule} from '@angular/core';
 
 import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
+import { SidebarComponent } from './view/sidebar/sidebar.component';
+import { DashboardComponent } from './view/dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SidebarComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
